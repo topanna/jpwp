aef55dd6fe13d9a7923d4763004b42849407df05
topanna <s171591@student.pg.edu.pl>
2020-11-07 17:47:48 +0100

    3

---
+++ stolice/src/stolice/Map.java
@@ -0,0 +1,132 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stolice;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.util.List;
+import java.awt.Point;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Ania
+ */
+public class Map extends JPanel implements MouseListener {
+
+    Image img = null;
+    Image blackPin = null;
+    Image redPin = null;
+    int x = 0;
+
+    List<Point> points;
+    int[] playerPoint = new int[2];
+    int[] actualPoint = new int[2];
+    
+    //int pin = 0;
+    
+    boolean mouseClicked = false;
+    boolean mouseReleased = false;
+
+    public Map() {
+        setPreferredSize(new Dimension(1024, 768));
+        setFocusable(true); //dddd
+        addMouseListener(this);
+        points = new ArrayList<Point>();
+        points.add(new Point(587, 408));
+        points.add(new Point(494, 409));
+        points.add(new Point(660, 360));
+
+    }
+
+    public void showPin(int x, int y, Image img, Graphics g) {
+
+       
+        if (mouseClicked) {
+            g.drawImage(img, x, y, null);
+        }
+
+    }
+    
+    public void showRedPin(int x, int y, Image img, Graphics g) {
+
+       
+        if (mouseReleased) {
+            g.drawImage(img, x, y, null);
+        }
+
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+
+        super.paintComponent(g);
+        try {
+            img = ImageIO.read(new File("map.png"));
+            blackPin = ImageIO.read(new File("pinblack.png"));
+            redPin = ImageIO.read(new File("pinred.png"));
+        } catch (IOException ex) {
+            Logger.getLogger(Map.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        g.drawImage(img, 0, 0, null);
+
+        showPin(playerPoint[0] - 12, playerPoint[1] - 38, blackPin, g);
+        showRedPin(actualPoint[0] - 12, actualPoint[1] - 38, redPin, g);
+        
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        mouseClicked = true;
+        playerPoint[0] = e.getX();
+        playerPoint[1] = e.getY();
+        actualPoint[0] = (int) points.get(0).getX();
+        actualPoint[1] = (int) points.get(0).getY();
+        
+        System.out.println(playerPoint[0] + ", " + playerPoint[1]);
+        System.out.println(actualPoint[0] + ", " + actualPoint[1]);
+        
+        repaint();
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        mouseReleased = true;
+//        try {
+//            Thread.sleep(2000);
+//        } catch (InterruptedException ex) {
+//            Logger.getLogger(Map.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+        repaint();
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+}
--- stolice/src/stolice/Start.java
+++ stolice/src/stolice/Start.java
@@ -9,48 +9,25 @@ package stolice;
  *
  * @author Ania
  */
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.Image;
 
-import javax.swing.ImageIcon;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 
 public class Start {
+    Graphics g;
 
-  public static void main(String[] args) {
-    ImagePanel panel = new ImagePanel(new ImageIcon("mapa5.png").getImage());
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Stolice Europy");
+        frame.setSize(new Dimension(1024, 768));
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Map map = new Map();
+        frame.add(map); //getcontentpane
+        frame.pack();
+        frame.setVisible(true);
 
-    JFrame frame = new JFrame("Stolice Europy");
-    frame.getContentPane().add(panel);
-    frame.pack();
-    frame.setVisible(true);
-    frame.setResizable(false);
-    frame.setLocationRelativeTo(null);
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-  }
-}
-
-class ImagePanel extends JPanel {
-
-  private Image img;
-
-  public ImagePanel(String img) {
-    this(new ImageIcon(img).getImage());
-  }
-
-  public ImagePanel(Image img) {
-    this.img = img;
-    Dimension size = new Dimension(img.getWidth(null), img.getHeight(null));
-    setPreferredSize(size);
-    //setMinimumSize(size);
-    //setSize(size);
-    setLayout(null);
-  }
-
-  public void paintComponent(Graphics g) {
-    g.drawImage(img, 0, 0, null);
-  }
-
+    }
 }
